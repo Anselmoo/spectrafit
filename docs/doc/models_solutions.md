0baafd4fa0d2130fd21645115c4a362a531009ac
Anselm Hahn <Anselm.Hahn@gmail.com>
2021-08-17 20:38:03 +0200

    Updated model section

---
+++ docs/doc/models_solutions.md
@@ -0,0 +1,15 @@
+## Solving of the curve models
+
+::: spectrafit.models.solver_model
+
+::: spectrafit.models.Constants
+
+## Implemented models
+
+::: spectrafit.models.gaussian
+
+::: spectrafit.models.lorentzian
+
+::: spectrafit.models.pseudovoigt
+
+::: spectrafit.models.voigt
--- spectrafit/models.py
+++ spectrafit/models.py
@@ -1,5 +1,5 @@
+"""Minimization models for curve fitting."""
 from dataclasses import dataclass
-from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -10,6 +10,32 @@ from scipy.special import wofz
 
 @dataclass(frozen=True)
 class Constants:
+    r"""Mathematical constants for the curve models.
+
+    !!! info "Constants"
+        $$
+        log2 = \log{2 }
+        $$
+
+        $$
+        sq2pi = \sqrt{2 \pi}
+        $$
+
+        $$
+        sqpi = \sqrt{ \pi}
+        $$
+
+        $$
+        sq2 = \sqrt{2 }
+        $$
+
+        $$
+        sig2fwhm = 2 \sqrt{2\log{2 }}
+        $$
+
+
+    """
+
     log2 = np.log(2.0)
     sq2pi = np.sqrt(2.0 * np.pi)
     sqpi = np.sqrt(np.pi)
@@ -17,16 +43,42 @@ class Constants:
     sig2fwhm = 2.0 * np.sqrt(2.0 * np.log(2.0))
 
 
-def solver_model(params: dict, x: np.array, data) -> np.array:
-    """[summary]
+def solver_model(params: dict, x: np.array, data: np.array) -> np.array:
+    """Solving the fitting problem.
+
+    !!! note "About implemented models"
+        `solver_model` is a wrapper function for the calling the implemented moldels.
+        Based on the `params` dictionary, the function calls the corresponding models
+        and merge them to the general model with will be optimized by the
+        `lmfit`-optimizer.
+        Currently the following models are supported:
+
+        - [Gaussian](https://en.wikipedia.org/wiki/Gaussian_function)
+        - [Lorentzian](https://en.wikipedia.org/wiki/Cauchy_distribution)
+            also known as Cauchy distribution
+        - [Voigt](https://en.wikipedia.org/wiki/Voigt_profile)
+        - [Pseudo Voigt]
+            (https://en.wikipedia.org/wiki/Voigt_profile#Pseudo-Voigt_approximation)
+        - Exponential
+        - [Powerlaw]
+            (https://en.wikipedia.org/wiki/Power_law) (also known as Log-parabola)
+        - Linear
+        - Constant
+        - [Error Function](https://en.wikipedia.org/wiki/Error_function)
+        - [Arcus Tangens](https://en.wikipedia.org/wiki/Inverse_trigonometric_functions)
+        - Logarithmic
+
 
     Args:
-        params (dict): [description]
-        x (np.array): [description]
-        data ([type]): [description]
+        params (dict): The best-fit parameters resulting from the fit.
+        x (np.array): `x`-values of the data.
+        data (np.array): `y`-values of the data, the data.
+
+    Raises:
+        SystemExit: If the model is not supported.
 
     Returns:
-        [type]: [description]
+        np.array: The best-fitted data based on the proposed model.
     """
     val = 0.0
     for model in params:
@@ -37,6 +89,7 @@ def solver_model(params: dict, x: np.array, data) -> np.array:
             "voigt",
             "pseudovoigt",
             "exponential",
+            "powerlaw",
             "linear",
             "constant",
             "erf",
@@ -151,7 +204,7 @@ def solver_model(params: dict, x: np.array, data) -> np.array:
                     amplitude=amplitude,
                     center=center,
                     sigma=sigma,
-                    form="erf",
+                    kind="erf",
                 )
         if "atan" in model:
             if "center" in model:
@@ -165,7 +218,7 @@ def solver_model(params: dict, x: np.array, data) -> np.array:
                     amplitude=amplitude,
                     center=center,
                     sigma=sigma,
-                    form="atan",
+                    kind="atan",
                 )
         if "log" in model:
             if "center" in model:
@@ -179,21 +232,27 @@ def solver_model(params: dict, x: np.array, data) -> np.array:
                     amplitude=amplitude,
                     center=center,
                     sigma=sigma,
-                    form="logistic",
+                    kind="logistic",
                 )
     return val - data
 
 
 def calculated_models(params: dict, x: np.array, df: pd.DataFrame) -> pd.DataFrame:
-    """[summary]
+    """Calculate the single contributions of the models and add them to the dataframe.
 
     Args:
-        params (dict): [description]
-        x (np.array): [description]
-        data ([type]): [description]
+        params (dict): The best optimized parameters of the fit.
+        x (np.array): `x`-values of the data.
+        df (pd.DataFrame): DataFrame containing the input data (`x` and `data`),
+             as well as the best fit and the corresponding residuum. Hence, it will be
+             extended by the single contribution of the model.
+
+    Raises:
+        SystemExit: If the model is not supported.
 
     Returns:
-        [type]: [description]
+        pd.DataFrame: Extended dataframe containing the single contributions of the
+             models.
     """
     for model in params:
         model = model.lower()
@@ -203,6 +262,7 @@ def calculated_models(params: dict, x: np.array, df: pd.DataFrame) -> pd.DataFra
             "voigt",
             "pseudovoigt",
             "exponential",
+            "powerlaw",
             "linear",
             "constant",
             "erf",
@@ -321,7 +381,7 @@ def calculated_models(params: dict, x: np.array, df: pd.DataFrame) -> pd.DataFra
                     amplitude=amplitude,
                     center=center,
                     sigma=sigma,
-                    form="erf",
+                    kind="erf",
                 )
         if "atan" in model:
             if "center" in model:
@@ -335,7 +395,7 @@ def calculated_models(params: dict, x: np.array, df: pd.DataFrame) -> pd.DataFra
                     amplitude=amplitude,
                     center=center,
                     sigma=sigma,
-                    form="atan",
+                    kind="atan",
                 )
         if "log" in model:
             if "center" in model:
@@ -349,14 +409,122 @@ def calculated_models(params: dict, x: np.array, df: pd.DataFrame) -> pd.DataFra
                     amplitude=amplitude,
                     center=center,
                     sigma=sigma,
-                    form="logistic",
+                    kind="logistic",
                 )
     return df
 
 
-def pv_cof(fwhm_g: float, fwhm_l: float) -> Tuple[float, float]:
+def gaussian(
+    x: np.array, amplitude: float = 1.0, center: float = 0.0, fwhm: float = 1.0
+) -> np.array:
+    r"""Return a 1-dimensional Gaussian distribution.
+
+    $$
+    {\displaystyle g(x)={\frac {1}{\sigma {\sqrt {2\pi }}}}\exp
+    (  -{\frac {1}{2}}{\frac {(x-\mu )^{2}}{\sigma ^{2}}} ) }
+    $$
+
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the Gaussian distribution. Defaults
+             to 1.0.
+        center (float, optional): Center of the Gaussian distribution. Defaults to 0.0.
+        fwhm (float, optional): Full width at half maximum (FWHM) of the Gaussian
+             distribution. Defaults to 1.0.
+
+    Returns:
+        np.array: Gaussian distribution of `x` given.
     """
-    Calculating the effectiv fwhm of the pseudo voigt profile and the fraction coefficient for n
+    sigma = fwhm / Constants.sig2fwhm
+    return (amplitude / (Constants.sq2pi * sigma)) * np.exp(
+        -((1.0 * x - center) ** 2) / (2 * sigma ** 2)
+    )
+
+
+def lorentzian(
+    x, amplitude: float = 1.0, center: float = 0.0, fwhm: float = 1.0
+) -> np.array:
+    r"""Return a 1-dimensional Lorentzian distribution.
+
+    $$
+    f(x;x_{0},\gamma )={\frac  {1}{\pi \gamma [ 1+ ( {\frac  {x-x_{0}}{\gamma }})^{2} ]
+    }} ={1 \over \pi \gamma } [ {\gamma ^{2} \over (x-x_{0})^{2}+\gamma ^{2}} ]
+    $$
+
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the Lorentzian distribution. Defaults
+             to 1.0.
+        center (float, optional): Center of the Lorentzian distribution. Defaults to
+             0.0.
+        fwhm (float, optional): Full width at half maximum (FWHM) of the Lorentzian
+             distribution. Defaults to 1.0.
+
+    Returns:
+        np.array: Lorentzian distribution of `x` given.
+    """
+    sigma = fwhm / 2.0
+    return (amplitude / (1 + ((1.0 * x - center) / sigma) ** 2)) / (np.pi * sigma)
+
+
+def voigt(
+    x: np.array, center: float = 0.0, fwhm: float = 1.0, gamma: float = None
+) -> np.array:
+    r"""Return a 1-dimensional Voigt distribution.
+
+    $$
+    {\displaystyle V(x;\sigma ,\gamma )\equiv \int _{-\infty }^{\infty }G(x';\sigma )
+    L(x-x';\gamma )\,dx'}
+    $$
+
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the Voigt distribution. Defaults to
+             1.0.
+        center (float, optional): Center of the Voigt distribution. Defaults to 0.0.
+        fwhm (float, optional): Full width at half maximum (FWHM) of the Lorentzian
+             distribution. Defaults to 1.0.
+        gamma (float, optional): Scaling factor of the complex part of the
+             [Faddeeva Function](https://en.wikipedia.org/wiki/Faddeeva_function).
+             Defaults to None.
+
+    Returns:
+        np.array: Voigt distribution of `x` given.
+    """
+    sigma = fwhm / 3.60131
+    if gamma is None:
+        gamma = sigma
+    z = (x - center + 1j * gamma) / (sigma * Constants.sq2)
+    return wofz(z).real / (sigma * Constants.sq2pi)
+
+
+def pseudovoigt(
+    x: np.array,
+    amplitude: float = 1.0,
+    center: float = 0.0,
+    fwhm_g: float = 1.0,
+    fwhm_l: float = 1.0,
+) -> np.array:
+    """Return a 1-dimensional Pseudo-Voigt distribution.
+
+    !!! note "See also:"
+
+        J. Appl. Cryst. (2000). 33, 1311-1316
+        https://doi.org/10.1107/S0021889800010219
+
+    Args:
+        x (np.array):  `x`-values of the data.
+        amplitude (float, optional): Amplitude of the Pseudo-Voigt distribution.
+             Defaults to 1.0.
+        center (float, optional): Center of the Pseudo-Voigt distribution.
+             Defaults to 0.0.
+        fwhm_g (float, optional): Full width half maximum of the Gaussian distribution
+            in the Pseudo-Voigt distribution. Defaults to 1.0.
+        fwhm_l (float, optional): Full width half maximum of the Lorentzian distribution
+            in the Pseudo-Voigt distribution. Defaults to 1.0.
+
+    Returns:
+        np.array: Pseudo-Voigt distribution of `x` given.
     """
     f = np.power(
         fwhm_g ** 5
@@ -372,95 +540,6 @@ def pv_cof(fwhm_g: float, fwhm_l: float) -> Tuple[float, float]:
         - 0.47719 * (fwhm_l / f) ** 2
         + 0.11116 * (fwhm_l / f) ** 3
     )
-    return (f, n)
-
-
-def gaussian(
-    x: np.array, amplitude: float = 1.0, center: float = 0.0, fwhm: float = 1.0
-) -> np.array:
-    """Return a 1-dimensional Gaussian function.
-
-    gaussian(x, amplitude, center, sigma) =
-        (amplitude/(sq2pi*sigma)) * exp(-(1.0*x-center)**2 / (2*sigma**2))
-
-    """
-    sigma = fwhm / Constants.sig2fwhm
-    return (amplitude / (Constants.sq2pi * sigma)) * np.exp(
-        -((1.0 * x - center) ** 2) / (2 * sigma ** 2)
-    )
-
-
-def lorentzian(
-    x, amplitude: float = 1.0, center: float = 0.0, fwhm: float = 1.0
-) -> np.array:
-    """Return a 1-dimensional Lorentzian function.
-
-    lorentzian(x, amplitude, center, sigma) =
-        (amplitude/(1 + ((1.0*x-center)/sigma)**2)) / (pi*sigma)
-
-    """
-    sigma = fwhm / 2.0
-    return (amplitude / (1 + ((1.0 * x - center) / sigma) ** 2)) / (np.pi * sigma)
-
-
-def voigt(
-    x: np.array, center: float = 0.0, fwhm: float = 1.0, gamma: float = None
-) -> np.array:
-    """Return a 1-dimensional Voigt function.
-
-    voigt(x, amplitude, center, sigma, gamma) =
-        amplitude*wofz(z).real / (sigma*sq2pi)
-
-    see http://en.wikipedia.org/wiki/Voigt_profile
-
-    """
-    sigma = fwhm / 3.60131
-    if gamma is None:
-        gamma = sigma
-    z = (x - center + 1j * gamma) / (sigma * Constants.s2)
-    return wofz(z).real / (sigma * Constants.sq2pi)
-
-
-# def pseudovoigt(x, amplitude=1.0, center=0.0, sigma=1.0, fraction=0.5):
-#    """Return a 1-dimensional pseudo-Voigt function.
-#
-#    pseudovoigt(x, amplitude, center, sigma, fraction) =
-#       amplitude*(1-fraction)*gaussion(x, center, sigma_g) +
-#       amplitude*fraction*lorentzian(x, center, sigma)
-#
-#    where sigma_g (the sigma for the Gaussian component) is
-#
-#        sigma_g = sigma / sqrt(2*log(2)) ~= sigma / 1.17741
-#
-#    so that the Gaussian and Lorentzian components have the
-#    same FWHM of 2*sigma.
-#
-#    """
-#    sigma_g = sigma / np.sqrt(2*log2)
-#    return ((1-fraction)*gaussian(x, amplitude, center, sigma_g) +
-#            fraction*lorentzian(x, amplitude, center, sigma))
-
-
-def pseudovoigt(
-    x: np.array,
-    amplitude: float = 1.0,
-    center: float = 0.0,
-    fwhm_g: float = 1.0,
-    fwhm_l: float = 1.0,
-) -> np.array:
-
-    # PSVOIGT This is a psuedo voigt function
-    #   This is programmed according to wikipidia and they cite:
-    #   Ida, T and Ando, M and Toraya, H (2000).
-    # "Extended pseudo-Voigt function for approximating the Voigt profile".
-    # Journal of Applied Crystallography 33 (6): 1311-1316.
-
-    # sigma_g = sigma / np.sqrt(2*log2)
-    f, n = pv_cof(fwhm_g, fwhm_l)
-
-    # f = np.power(fwhm_g**5 +2.69269*fwhm_g**4*fwhm_l+2.42843*fwhm_g**3*fwhm_l**2+
-    #             4.47163*fwhm_g**2*fwhm_l**3+0.07842*fwhm_g*fwhm_l**4+fwhm_l**5,0.25)
-    # n = 1.36603*(fwhm_l/f) - 0.47719*(fwhm_l/f)**2 +0.11116*(fwhm_l/f)**3
     return n * lorentzian(x, amplitude, center, fwhm_l) + (1 - n) * gaussian(
         x, amplitude, center, fwhm_g
     )
@@ -469,10 +548,18 @@ def pseudovoigt(
 def exponential(
     x: np.array, amplitude: float = 1.0, decay: float = 1.0, intercept: float = 0.0
 ) -> np.array:
-    """Return an exponential function.
+    """Return a 1-dimensional exponential decay.
 
-    x -> amplitude * exp(-x/decay)
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the exponential function. Defaults to
+             1.0.
+        decay (float, optional): Decay of the exponential function. Defaults to 1.0.
+        intercept (float, optional): Intercept of the exponential function. Defaults to
+             0.0.
 
+    Returns:
+        np.array: Exponential decay of `x` given.
     """
     return amplitude * np.exp(-x / decay) + intercept
 
@@ -480,29 +567,45 @@ def exponential(
 def powerlaw(
     x: np.array, amplitude: float = 1.0, exponent: float = 1.0, intercept: float = 0.0
 ) -> np.array:
-    """Return the powerlaw function.
+    """Return a 1-dimensional powerlaw function.
 
-    x -> amplitude * x**exponent
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the powerlaw function. Defaults to
+             1.0.
+        exponent (float, optional): Exponent of the powerlaw function. Defaults to 1.0.
+        intercept (float, optional): Intercept of the powerlaw function. Defaults to
+             0.0.
 
+    Returns:
+        np.array: Powerlaw function of `x` given.
     """
-
     return amplitude * np.power(x, exponent) + intercept
 
 
 def linear(x: np.array, slope: float = 1.0, intercept: float = 0.0) -> np.array:
-    """Return a linear function.
+    """Return a 1-dimensional linear function.
 
-    x -> slope * x + intercept
+    Args:
+        x (np.array): `x`-values of the data.
+        slope (float, optional): Slope of the linear function. Defaults to 1.0.
+        intercept (float, optional): Intercept of the linear function. Defaults to 0.0.
 
+    Returns:
+        np.array: Linear function of `x` given.
     """
     return slope * x + intercept
 
 
 def constant(x: np.array, amplitude: float = 1.0) -> np.array:
-    """Return a cosntant function.
+    """Returns a 1-dimensional constant value.
 
-    x -> constant
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the constant. Defaults to 1.0.
 
+    Returns:
+        np.array: Constant value of `x` given.
     """
     return np.linspace(amplitude, amplitude, len(x))
 
@@ -512,29 +615,40 @@ def step(
     amplitude: float = 1.0,
     center: float = 0.0,
     sigma: float = 1.0,
-    form: str = "linear",
+    kind: str = "linear",
 ) -> np.array:
-    """Return a step function.
+    r"""Returns a 1-dimensional step function.
 
-    starts at 0.0, ends at amplitude, with half-max at center, and
-    rising with form:
-      'linear' (default) = amplitude * min(1, max(0, arg))
-      'atan', 'arctan'   = amplitude * (0.5 + atan(arg)/pi)
-      'erf'              = amplitude * (1 + erf(arg))/2.0
-      'logistic'         = amplitude * [1 - 1/(1 + exp(arg))]
+    Args:
+        x (np.array): `x`-values of the data.
+        amplitude (float, optional): Amplitude of the step function. Defaults to 1.0.
+        center (float, optional): Cener of the step function. Defaults to 0.0.
+        sigma (float, optional): Sigma of the step function. Defaults to 1.0.
+        kind (str, optional): Kind of the step function; can be 'linear',
+             'atan' or 'arctan', 'log' or 'logistic', 'erf'. Defaults to "linear".
 
-    where arg = (x - center)/sigma
+    Returns:
+        np.array: Step function of `x` given.
 
+
+    !!! note "Available step functions"
+        ```python
+        step_linear = amplitude * min(1, max(0, arg))
+        step_atan = amplitude * (0.5 + atan(arg) / pi)
+        step_erf = amplitude * (1 + erf(arg)) / 2.0
+        step_log = amplitude * [1 - 1 / (1 + exp(arg))]
+        # where arg = (x - center)/sigma
+        ```
     """
     if abs(sigma) < 1.0e-13:
         sigma = 1.0e-13
 
     out = (x - center) / sigma
-    if form == "erf":
+    if kind == "erf":
         out = 0.5 * (1 + erf(out))
-    elif form.startswith("logi"):
+    elif kind.startswith("log"):
         out = 1.0 - 1.0 / (1.0 + np.exp(out))
-    elif form in {"atan", "arctan"}:
+    elif kind in {"atan", "arctan"}:
         out = 0.5 + np.arctan(out) / np.pi
     else:
         out[np.where(out < 0)] = 0.0
