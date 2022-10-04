80ffb4c9da91074fe696ec9970c5742fc829e479
Anselm Hahn <Anselm.Hahn@gmail.com>
2022-10-04 13:39:35 +0000

    fix: :green_heart: Complete poetry install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 python -m pip install --upgrade pip
 python -m pip install poetry
 poetry config virtualenvs.in-project true
-python -m poetry install -E jupyter
+python -m poetry install --with dev,docs --all-extras
