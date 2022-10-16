9600e4fee815938a6f4ff584fbc25ff770b45661
Anselm Hahn <Anselm.Hahn@gmail.com>
2022-10-16 16:55:08 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,3 @@ python -m pip install --upgrade pip
 python -m pip install poetry
 poetry config virtualenvs.in-project true
 python -m poetry install --with dev,docs --all-extras
-sudo chown -R $(whoami) /home/linuxbrew/.linuxbrew/Homebrew
-brew install git-flow-avh
-brew install prettier
