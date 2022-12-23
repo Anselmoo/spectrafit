fbde19119317d1efa6f3625dc00d27677982e49a
Anselm Hahn <Anselm.Hahn@gmail.com>
2022-12-23 16:36:27 +0100

    fix: :test_tube: Correct type defs

--- spectrafit/api/test/test_file_model.py
+++ spectrafit/api/test/test_file_model.py
@@ -4,98 +4,120 @@ import pytest
 from spectrafit.api.file_model import DataFileAPI
 
 
-def test_delimiter() -> None:
+def test_delimiter_space() -> None:
     """Test the delimiter validator."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter=" ",
+        file_suffixes=[".txt"],
     )
     assert data_file.delimiter == " "
 
+
+def test_delimiter_tab() -> None:
+    """Test the delimiter validator for tab separation."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter="\t",
+        file_suffixes=[".txt"],
     )
     assert data_file.delimiter == "\t"
 
+
+def test_delimiter_comma() -> None:
+    """Test the delimiter validator for comma separation."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter=",",
+        file_suffixes=[".txt"],
     )
     assert data_file.delimiter == ","
 
+
+def test_delimiter_semicolon() -> None:
+    """Test the delimiter validator for semicolon separation."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter=";",
+        file_suffixes=[".txt"],
     )
     assert data_file.delimiter == ";"
 
+
+def test_delimiter_pipe() -> None:
+    """Test the delimiter validator for pipe separation."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter="|",
+        file_suffixes=[".txt"],
     )
     assert data_file.delimiter == "|"
 
+
+def test_delimiter_regex() -> None:
+    """Test the delimiter validator for regex separation."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
-        delimiter="s+",
+        skiprows=0,
+        skipfooter=0,
+        delimiter=r"\s+",
+        file_suffixes=[".txt"],
     )
-    assert data_file.delimiter == "s+"
+    assert data_file.delimiter == r"\s+"
 
 
-def test_delimiter_error() -> None:
-    """Test the delimiter validator."""
-    with pytest.raises(ValueError) as excinfo:
+def test_delimiter_regex_error() -> None:
+    """Test the delimiter validator for regex error."""
+    with pytest.raises(ValueError):
         DataFileAPI(
-            skip_header=0,
-            skip_footer=0,
-            delimiter="x",
+            skiprows=0,
+            skipfooter=0,
+            delimiter=r"\s",
+            file_suffixes=[".txt"],
         )
 
-    assert "Delimiter not supported." in str(excinfo.value)
 
-
-def test_comment_marker() -> None:
+def test_comment() -> None:
     """Test the comment marker validator."""
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter=" ",
-        comment_marker="#",
+        comment="#",
+        file_suffixes=[".txt"],
     )
-    assert data_file.comment_marker == "#"
+    assert data_file.comment == "#"
 
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter=" ",
-        comment_marker="%",
+        comment="%",
+        file_suffixes=[".txt"],
     )
-    assert data_file.comment_marker == "%"
+    assert data_file.comment == "%"
 
     data_file = DataFileAPI(
-        skip_header=0,
-        skip_footer=0,
+        skiprows=0,
+        skipfooter=0,
         delimiter=" ",
-        comment_marker=None,
+        comment=None,
+        file_suffixes=[".txt"],
     )
-    assert data_file.comment_marker is None
+    assert data_file.comment is None
 
 
-def test_comment_marker_error() -> None:
-    """Test the comment marker validator."""
-    with pytest.raises(ValueError) as excinfo:
+def test_comment_error() -> None:
+    """Test the comment marker validator for error."""
+    with pytest.raises(ValueError):
         DataFileAPI(
-            skip_header=0,
-            skip_footer=0,
+            skiprows=0,
+            skipfooter=0,
             delimiter=" ",
-            comment_marker="x",
+            comment="x",
+            file_suffixes=[".txt"],
         )
-
-    assert "Comment marker not supported." in str(excinfo.value)
