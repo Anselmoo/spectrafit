6484678e4cd1f93699a8d26b9ef6f064ea7d3f58
Anselm Hahn <Anselm.Hahn@gmail.com>
2023-05-01 15:36:03 +0200

    Revert "feat: :wrench: Get current python version from `pyproject.toml`"
    This reverts commit 8add8edb4becaac5ca8b1d9db90041f4f978ce3b.

--- spectrafit/__init__.py
+++ spectrafit/__init__.py
@@ -1,9 +1,2 @@
 """SpectraFit, fast command line tool for fitting data."""
-from pathlib import Path
-
-import tomli
-
-
-with open(Path(__file__).parent.parent / "pyproject.toml", "rb") as f:
-    pyproject = tomli.load(f)
-    __version__ = pyproject["tool"]["poetry"]["version"]
+__version__ = "0.16.8"
