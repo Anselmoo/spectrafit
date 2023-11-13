c133c38f1a096a20314ac48a1d1b726be9356433
Anselm Hahn <Anselm.Hahn@gmail.com>
2023-11-13 16:28:30 +0100

    perf: :rotating_light: Rebuild fond by `findfont`-method

--- spectrafit/__init__.py
+++ spectrafit/__init__.py
@@ -1,2 +1,22 @@
-"""SpectraFit, fast command line tool for fitting data."""
+"""SpectraFit, fast command line tool for fitting data.
+
+!!! info "About Versioning"
+
+    SpectraFit uses [Semantic Versioning](https://semver.org/).
+
+!!! info "About the Font Cache"
+
+    For avoiding problems with the font cache, the font cache is rebuilt at the
+    beginning of the program. This can take a few seconds. If you want to avoid
+    this, you can comment out the line `matplotlib.font_manager._rebuild()` in
+    the `__init__.py` file.
+
+"""
+
+
 __version__ = "1.0.0"
+
+import matplotlib.font_manager
+
+
+matplotlib.font_manager.findfont("serif", rebuild_if_missing=True)
