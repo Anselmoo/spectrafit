12603f7a5850642368ad3ed35bbf80edcb81e337
Anselm Hahn <Anselm.Hahn@gmail.com>
2024-01-20 18:38:48 +0100

    refactor: :art: Rebuild font cache at program start

--- spectrafit/__init__.py
+++ spectrafit/__init__.py
@@ -3,20 +3,7 @@
 !!! info "About Versioning"
 
     SpectraFit uses [Semantic Versioning](https://semver.org/).
-
-!!! info "About the Font Cache"
-
-    For avoiding problems with the font cache, the font cache is rebuilt at the
-    beginning of the program. This can take a few seconds. If you want to avoid
-    this, you can comment out the line `matplotlib.font_manager._rebuild()` in
-    the `__init__.py` file.
-
 """
 
 
 __version__ = "1.0.0"
-
-import matplotlib.font_manager
-
-
-matplotlib.font_manager.findfont("serif", rebuild_if_missing=True)
