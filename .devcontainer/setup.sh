e35d4915b068c721e1c9b7da251a6b6b011ccc71
Anselm Hahn <Anselm.Hahn@gmail.com>
2025-01-22 07:37:28 +0100

    feat: :art: update devcontainer configuration to use latest features and install uv for environment setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 python -m pip install --upgrade pip
-python -m pip install poetry
-poetry config virtualenvs.in-project true
-python -m poetry install --with dev,docs --all-extras
+python -m pip install uv
+uv venv --python 3.10
+uv sync --all-extras --all-groups
